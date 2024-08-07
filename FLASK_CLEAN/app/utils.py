2c6f48d3fe67fc21fa41d44210ee5c74849531f0
Pedro Henrique <123014696+PedroHenrique2107@users.noreply.github.com>
2024-08-07 20:36:47 -0300

    Status atualizado e completo da documentação do projeto

--- FLASK_CLEAN/app/utils.py
+++ FLASK_CLEAN/app/utils.py
@@ -2,6 +2,14 @@
 import random
 
 def get_sensor_data():
-    temperature = round(random.uniform(15.0, 30.0), 2)  # Temperatura entre 15 e 30 graus Celsius
-    humidity = round(random.uniform(30.0, 70.0), 2)  # Umidade entre 30% e 70%
-    return temperature, humidity
+    """
+    Gera dados simulados de sensores para temperatura e umidade.
+    A função simula a leitura de dados de sensores para temperatura e umidade,
+    gerando valores aleatórios dentro de um intervalo predefinido. Esses valores
+    são arredondados para duas casas decimais.
+
+    """
+   
+    temperature = round(random.uniform(15.0, 30.0), 2)  # Gera um valor de temperatura aleatório entre 15.0 e 30.0 graus Celsius
+    humidity = round(random.uniform(30.0, 70.0), 2) # Gera um valor de umidade aleatório entre 30.0% e 70%
+    return temperature, humidity # Retorna os valores de temperatura e umidade como uma tupla
