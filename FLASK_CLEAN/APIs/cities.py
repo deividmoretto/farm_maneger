1f3c2bd3c0853da5d655b2ba9887354b6a107dac
deividmoretto <75542165+deividmoretto@users.noreply.github.com>
2024-08-13 20:13:26 -0300

    Alteração

--- FLASK_CLEAN/APIs/cities.py
+++
@@ -1,8 +0,0 @@
-# Lista de cidades disponíveis para seleção
-CITIES = [
-    {"id": 1, "name": "London", "country": "UK"},
-    {"id": 2, "name": "New York", "country": "USA"},
-    {"id": 3, "name": "Tokyo", "country": "Japan"},
-    {"id": 4, "name": "São Paulo", "country": "Brazil"},
-    # Adicione mais cidades conforme necessário
-]
--- FLASK_CLEAN/app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
-
-class AddCropForm(FlaskForm):
-    name = StringField('Nome da Cultura', validators=[DataRequired()])
-    description = TextAreaField('Descrição', validators=[DataRequired()])
-    submit = SubmitField('Adicionar Cultura')
