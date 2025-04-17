0449d8c7c20501ae6f1c04139cc7fb7a0f2d8633
deividmoretto <75542165+deividmoretto@users.noreply.github.com>
2025-04-17 12:41:31 -0300

    exclusao de antigos

--- eder/flask 2/AgroLink/app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class LoginForm(FlaskForm):
-    email = EmailField("Email", validators=[DataRequired()])
-    senha = PasswordField("Senha", validators=[DataRequired(), Length(min=3, max=16)])
-    remember = BooleanField("Permanecer Conectado")
-    submit = SubmitField("Entrar")
